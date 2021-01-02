e3f1eb4e2d4b1511c4d02b1428c09c6a483d9b80
relaxseason <relax.season@gmail.com>
2021-01-02 19:32:53 +0900

    81マス版M9フォーマット作成

--- src/M9/A4-M81B.py
+++ src/M9/A4-M81B.py
@@ -3,6 +3,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 from reportlab.lib import colors
 from logging import getLogger
+import math
 logger = getLogger(__name__)
 
 
@@ -24,8 +25,9 @@ a4height_mm = a4height*mm
 pdfFile.setPageSize((a4width_mm, a4height_mm))
 
 # ラインの色指定
-# pdfFile.setFillColor(colors.green)
-pdfFile.setStrokeColor(colors.blue)
+linecolor = colors.blue
+# linecolor = colors.green
+pdfFile.setStrokeColor(linecolor)
 # pdfFile.setFillColorRGB(128, 128, 128)
 
 # ラインの太さを変更する
@@ -35,50 +37,70 @@ pdfFile.setLineWidth(0.1*mm)
 
 
 # 表の描画
+xbindingSpace = 11
+ybindingSpace = xbindingSpace-1
+cellsize = (a4width - xbindingSpace*2)/9
+xmargin = (a4width - (cellsize * 9))/2
+ymargin = (a4height - (cellsize * 9))/2
 
-cellwidth = 21.5
-base_x_space = (a4width - (cellwidth * 9))/2
-base_y_space = (a4height - (cellwidth * 9))/2
+# 切り取り線
+top = a4height - ymargin + ybindingSpace
+bottom = ymargin - ybindingSpace
+pdfFile.line(0, top * mm, a4width * mm, top * mm)
+pdfFile.line(0, bottom * mm, a4width * mm, bottom * mm)
+
 # 8個の曼陀羅
-base_points = [{"x": base_x_space + cellwidth * 0 * 3,
-                "y": base_y_space + cellwidth * 0 * 3},
-               {"x": base_x_space + cellwidth * 1 * 3,
-                "y": base_y_space + cellwidth * 0 * 3},
-               {"x": base_x_space + cellwidth * 2 * 3,
-                "y": base_y_space + cellwidth * 0 * 3},
-               {"x": base_x_space + cellwidth * 0 * 3,
-                "y": base_y_space + cellwidth * 1 * 3},
-               # {"x": base_x_space + cellwidth * 1 * 3,
-               # "y": base_y_space + cellwidth * 1 * 3},
-               {"x": base_x_space + cellwidth * 2 * 3,
-                "y": base_y_space + cellwidth * 1 * 3},
-               {"x": base_x_space + cellwidth * 0 * 3,
-                "y": base_y_space + cellwidth * 2 * 3},
-               {"x": base_x_space + cellwidth * 1 * 3,
-                "y": base_y_space + cellwidth * 2 * 3},
-               {"x": base_x_space + cellwidth * 2 * 3,
-                "y": base_y_space + cellwidth * 2 * 3},
-               ]
-for base in base_points:
-    x_space = base["x"]
-    y_space = base["y"]
-    xlist = ((x_space + cellwidth * 0) * mm, (x_space + cellwidth * 1) *
-             mm, (x_space + cellwidth * 2) * mm, (x_space + cellwidth * 3) * mm)
-    ylist = ((y_space + cellwidth * 0) * mm, (y_space + cellwidth * 1) *
-             mm, (y_space + cellwidth * 2) * mm, (y_space + cellwidth * 3) * mm)
+outBox = [(x, y)
+          for x in range(3)
+          for y in range(3)
+          if(not(x == 1 & y == 1))]
+
+for x, y in [(xmargin + cellsize * x * 3,
+              ymargin + cellsize * y * 3)
+             for x, y in outBox]:
+    xlist = [(x + cellsize * i) * mm for i in [0, 1, 2, 3]]
+    ylist = [(y + cellsize * i) * mm for i in [0, 1, 2, 3]]
+
+    pdfFile.setLineWidth(0.1*mm)
     pdfFile.grid(xlist, ylist)
+
+    pdfFile.setLineWidth(1)
+    pdfFile.rect((x+cellsize)*mm, (y+cellsize)*mm,
+                 cellsize*mm, cellsize*mm)
+
+# 外側の箱の区切り線
+pdfFile.setLineWidth(1)
+for i in [i*3*cellsize for i in [1, 2]]:
+    pdfFile.line((xmargin + i) * mm, ymargin * mm,
+                 (xmargin + i) * mm, (ymargin + cellsize * 9) * mm)
+    pdfFile.line(xmargin * mm, (ymargin + i) * mm,
+                 (xmargin + cellsize * 9) * mm, (ymargin + i) * mm)
 
 # 中央の曼陀羅
 # ラインの太さを変更する
 pdfFile.setLineWidth(1)
-x_space = base_x_space
-y_space = base_y_space
-xlist = ((x_space + cellwidth * 3) * mm, (x_space + cellwidth * 4) * mm,
-         (x_space + cellwidth * 5) * mm,  (x_space + cellwidth * 6) * mm)
-ylist = (
-    (y_space + cellwidth * 3) * mm, (y_space + cellwidth * 4) * mm,
-    (y_space + cellwidth * 5) * mm, (y_space + cellwidth * 6) * mm)
+x_space = xmargin
+y_space = ymargin
+xlist = [(x_space + cellsize * (i+3)) * mm for i in range(4)]
+ylist = [(y_space + cellsize * (i+3)) * mm for i in range(4)]
 pdfFile.grid(xlist, ylist)
+
+# 余白のドット方眼
+pdfFile.setFillColor(linecolor)
+dotspace1st = 1.25
+for x, y in [((x)*dotspace1st, (y)*dotspace1st)
+             for x in range(math.ceil(a4width/dotspace1st))
+             for y in range(math.ceil((ymargin-ybindingSpace)/dotspace1st))]:
+    pdfFile.circle(x*mm, y*mm, 0.15*mm, stroke=0, fill=1)
+    pdfFile.circle(x*mm, (top+y)*mm, 0.15*mm, stroke=0, fill=1)
+
+dotspace2nd = 5
+for x, y in [((x)*dotspace2nd, (y)*dotspace2nd)
+             for x in range(math.ceil(a4width/dotspace2nd))
+             for y in range(math.ceil((ymargin-ybindingSpace)/dotspace2nd))]:
+    pdfFile.circle(x*mm, y*mm, 0.25*mm, stroke=0, fill=1)
+    pdfFile.circle(x*mm, (top+y)*mm, 0.25*mm, stroke=0, fill=1)
+
 
 pdfFile.restoreState()
 pdfFile.save()
